42596349adbeeb4a0f3da5a77b081634d471f9f0
ｷﾞｮｴｰｰｰｰｰｰｰｯ <105195234+ononarumi@users.noreply.github.com>
2023-07-11 01:19:29 +0900

    2

--- docs/script.js
+++ docs/script.js
@@ -1,14 +1,27 @@
-$(document).ready(function() {
-    $("#button").click(function() {
-      $("#text").fadeIn();
-      $('html, body').animate({
-        scrollTop: $("#text").offset().top
-      }, 1000);
+
+  $(document).ready(function() {
+    $("#weatherImage2").click(function() {
+      $("#fadeInImage2").fadeIn();
+      $("#weatherImage1").fadeOut();
+      $("#harewrap").css("padding-top", "400px");
+      $("#weatherImage2").css("max-width","15%");
+      $("#harewrap").css("align-items", "center");
+      $(" #fadeInImage2").css("padding-right","15%");
+      
     });
+  });
   
-    // テキストの表示後に縦書きスタイルを適用する
-    $("#text").on("animationend", function() {
-      $(this).addClass("vertical-text");
+  $(document).ready(function() {
+    $("#weatherImage1").click(function() {
+      $("#fadeInImage1").fadeIn();
+      $("#weatherImage2").fadeOut();
+      $("#amewrap").css("padding-top", "150%");
+      $("#weatherImage1").css("max-width","13%");
+      $("#weatherImage1").css("padding-left","60%");
+      $("#amewrap").css("align-items", "center");
+      $(" #fadeInImage1").css("padding-left","5%");
+      $(" #fadeInImage1").css("max-width","100%")
+      
     });
   });
   
